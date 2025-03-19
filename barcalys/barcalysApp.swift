8967a8f9b060d9893d221873f624c5d1bfbe6d86
tajhassan <mdtaj.hassan@zensar.com>
2025-03-19 11:28:39 +0530

    Initial Commit

---
+++ barcalys/barcalysApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcalysApp.swift
+//  barcalys
+//
+//  Created by tajhassan on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct barcalysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
